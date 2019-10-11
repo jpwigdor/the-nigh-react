c7aaf2c86a644f4943c7867381da977da7c92fd0
James Wigdor <jpwigdor@gmail.com>
2019-10-10 21:58:50 -0400

    Homepage Titles

--- the-nigh/src/components/Main.js
+++ the-nigh/src/components/Main.js
@@ -3,7 +3,15 @@ import styled from "styled-components";
 
 class Main extends Component {
 	render() {
-		return <MainWrapper>MAIN COMPONENT</MainWrapper>;
+		return (
+			<MainWrapper>
+				MAIN COMPONENT
+				<h2>News Placemarker</h2>
+				<h2>Who are we?</h2>
+				<h2>Setlists for our shows</h2>
+				<h2>What do we listen to?</h2>
+			</MainWrapper>
+		);
 	}
 }
 
